9583228b58cbcaf2197985ad900623ec87b99730
Eshwar Chandra <eshwarcc@gmail.com>
2013-05-18 21:11:10 +0530

    Initial Commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+
+	def index
+		@home = "See It Later Home"
+	end
+	
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seeitlater::Application.config.secret_token = 'aeb98f6cb367684480eacc51f35292d0aa76bb11fe3af886c375d76d622437c28a30cec81198f7f349dba6ae5a1b973d2329b1bc600813e4eb6cbd1e9e6db084'
